20f84b30535053286cde3d24f40e07ffd955070b
Steve Banton <steve.g.banton@gmail.com>
2017-10-19 16:52:13 -0400

    Tweak me.jsx

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -13,12 +13,7 @@ const Me = ({ }) => (
     </div>
     <div id="me-description">
       <div>
-<<<<<<< HEAD
         I’m a developer from Toronto. I like to create business-centered applications using <a href="http://meteor.com" target="new"><span className="bold-font hover-yellow">Meteor <span className="material-icons">open_in_new</span></span></a> & <a href="http://reactjs.org" target="new"><span className="bold-font hover-yellow">React <span className="material-icons">open_in_new</span></span></a>. I'm also a big fan of <a href="http://material.io" target="new"><span className="bold-font hover-yellow">Material Design <span className="material-icons">open_in_new</span></span></a>.
-=======
-        I’m a developer from Toronto. I like to create business-centered applications using <a href="http://meteor.com" target="new"><span className="bold-font hover-yellow">Meteor <span className="material-icons">open_in_new</span></span></a> & <a href="http://reactjs.org" target="new"><span className="bold-font hover-yellow">React <span className="material-icons">open_in_new</span></span></a>. I'm also a big fan of
-        <a href="http://material.io" target="new"><span className="bold-font hover-yellow"> Material Design <span className="material-icons">open_in_new</span></span></a>.
->>>>>>> caac72b1d3219f10f4ee3d8ebc9a4084f5423bff
       </div>
     </div>
   </div>
