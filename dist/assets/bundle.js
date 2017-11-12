d1bb7743257504d5be775c6bd61e03dad2c1ef98
Steve Banton <steve.g.banton@gmail.com>
2017-11-12 14:26:28 -0500

    Adjust portfolio items

--- dist/assets/bundle.js
+++ dist/assets/bundle.js
@@ -35380,19 +35380,19 @@ var projects = [{
   link: 'https://yagosite.herokuapp.com/',
   github: 'https://github.com/SteveGBanton/yago'
 }, {
-  title: 'Recipe Box',
-  image: 'images/portfolio/recipe.jpg',
-  description: 'An editable recipe box created with React, Redux and Material Design. Add and edit recipes, with functionality to quickly add and delete ingredients one by one.',
-  tags: ['react', 'redux'],
-  link: 'http://stevebanton.com/recipe-box',
-  github: 'https://github.com/SteveGBanton/recipe-box'
-}, {
   title: 'Animated Link Graph: All Countries Sharing A Border',
   image: 'images/portfolio/force-directed.jpg',
   description: 'A D3.js force directed graph of states that share a land or water border.',
   tags: ['d3'],
   link: 'http://stevebanton.com/force-graph-countries',
   github: 'https://github.com/SteveGBanton/force-graph-countries'
+}, {
+  title: 'Recipe Box',
+  image: 'images/portfolio/recipe.jpg',
+  description: 'An editable recipe box created with React, Redux and Material Design. Add and edit recipes, with functionality to quickly add and delete ingredients one by one.',
+  tags: ['react', 'redux'],
+  link: 'http://stevebanton.com/recipe-box',
+  github: 'https://github.com/SteveGBanton/recipe-box'
 }, {
   title: 'Instant Markdown Previewer',
   image: 'images/portfolio/markdown.jpg',
@@ -35408,13 +35408,6 @@ var projects = [{
   link: 'http://stevebanton.com/simon-responsive',
   github: 'https://github.com/SteveGBanton/simon-responsive'
 }, {
-  title: 'Wikipedia Search Page',
-  image: 'images/portfolio/wikipedia.jpg',
-  description: 'A Wikipedia search page that uses the Wikipedia API to call search results before leaving the site.',
-  tags: ['jquery'],
-  link: 'https://codepen.io/stevesacct/full/OWdaGe/',
-  github: ''
-}, {
   title: 'Heatmap Graph: Global Land Surface Temperatures',
   image: 'images/portfolio/heatmap.jpg',
   description: 'An reuasable, animated D3.js heatmap graph of monthly land surface temperature data graphed from 1753 - 2015, with data tooltip on hover.',
@@ -35428,13 +35421,6 @@ var projects = [{
   tags: ['d3'],
   link: 'http://stevebanton.com/bar-graph-us-gdp',
   github: 'https://github.com/SteveGBanton/bar-graph-us-gdp'
-}, {
-  title: 'Pomodoro Clock',
-  image: 'images/portfolio/pomodoro.jpg',
-  description: 'A simple pomodoro clock that allows you to add minutes on the fly.',
-  tags: ['jquery'],
-  link: 'https://codepen.io/stevesacct/pen/jBqyOz',
-  github: ''
 }, {
   title: 'Dynamic Scatterplot Graph: Doping in Pro Cycling',
   image: 'images/portfolio/scatter.jpg',
@@ -35450,25 +35436,18 @@ var projects = [{
   link: 'http://stevebanton.com/world-plot',
   github: 'https://github.com/SteveGBanton/world-plot'
 }, {
-  title: 'Timestamp Service',
-  image: 'images/portfolio/timestamp.jpg',
-  description: 'A simple timestamp service that returns a time in unix and standard format when either are supplied.',
-  tags: ['node', 'express'],
-  link: 'https://simple-timestamp-service.herokuapp.com/',
-  github: 'https://github.com/SteveGBanton/timestamp-service'
+  title: 'StallionMotorSports.Com',
+  image: 'images/portfolio/stallion.jpg',
+  description: 'A website for a Top Sportsman drag racing team specializing in Pontiacs.',
+  tags: ['wordpress'],
+  link: 'http://StallionMotorSports.com',
+  github: ''
 }, {
   title: 'BoldPointStudio.com',
   image: 'images/portfolio/boldpoint.jpg',
   description: 'Responsive single page website for Boldpoint Studio, a design and marketing firm based in Toronto. Website built from scratch using jQuery.',
   tags: ['jquery'],
   link: 'http://www.boldpointstudio.com',
-  github: ''
-}, {
-  title: 'AlbionLandscaping.Net',
-  image: 'images/portfolio/albion.jpg',
-  description: 'A website for a local landscaping business in the Toronto area, built using Wordpress.',
-  tags: ['wordpress'],
-  link: 'http://AlbionLandscaping.Net',
   github: ''
 }];
 
